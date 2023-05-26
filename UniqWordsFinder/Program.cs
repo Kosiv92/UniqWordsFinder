147c44d5f6fbccf0863a9f2f97096a6390746f95
kosiv92 <kosiv92@gmail.com>
2023-05-27 01:33:47 +0800

    Add WebAPI

--- UniqWordsFinder/Program.cs
+++ UniqWordsFinder/Program.cs
@@ -6,18 +6,26 @@ using System.Diagnostics;
 namespace UniqWordsFinder
 {
     internal class Program
-    {        
+    {
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var fileReader = new FileReader(InteractionMethods.InputPathToFile()); //считываем файл
-            IDataHandler dataHandler = new RawDataHandler(fileReader.GetReadResultInLines()); //передаем результат обработчику
-            var client = new ConsoleClient(dataHandler);
 
-            //для вызова метода путем рефлексии
-            //client.CallPrivateMethod(); 
+            var webClient = new WebServiceClient("https://localhost:7217/Text");
+            if (!webClient.IsServerAvailable().Result)
+            {
+                Console.WriteLine("Server is not available. Application will be closed");
+                Console.ReadKey();
+                Environment.Exit(0);
+            };
+
+            IDataHandler dataHandler = new WebDataHandler(webClient, fileReader.GetReadResultInLines());                        
                         
-            client.Execute();            
-        }                
+            //IDataHandler dataHandler = new RawDataHandler(fileReader.GetReadResultInLines()); //передаем результат обработчику
+            var client = new ConsoleClient(dataHandler);                       
+
+            client.Execute();
+        }
     }
 }
